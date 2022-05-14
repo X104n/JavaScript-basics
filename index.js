377db8f5431394bfaadbe85ff9fc4be7d473f4b8
Stian Munkejord <smu027@uib.no>
2022-05-14 16:11:43 +0200

    Js objects

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 let name = 'Stan ';
+const rate = 1;
 console.log(name);
+
+let person = {
+    name: 'Stian',
+    age: 20
+};
+
+person.name = 'John';
+
+let selection = 'name';
+person[selection] = 'Mary';
+
+console.log(person.name)
